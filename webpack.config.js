eb2dddc9b6a337fdb5ea643603cc8daab7bd24ee
Sergey Tkachenko <tkachenkosi@gmail.com>
2022-04-27 12:25:13 +0300

    Установка и настройка Bavel

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,13 @@ module.exports = {
       patterns: [{from: 'public'}],
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      }
+    ]
+  }
 };
